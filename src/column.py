9f86b5be13b7973fc2f5bde0b60c152e2df34439
Raphaël Castanier <raphael.castanier@free.fr>
2023-08-10 10:30:58 +0200

    feat: add column

---
+++ src/column.py
@@ -0,0 +1,32 @@
+# Column
+#
+# A Column has a Direction and a Card stack
+
+import unittest
+
+from card import Card
+from direcion import Direction
+
+
+class Column:
+    def __init__(self, direction: Direction) -> None:
+        self.direction = direction
+        self.stack = list()
+
+    def play(self, card: Card) -> None:
+        self.stack.append(card)
+
+    def top(self) -> Card:
+        return self.stack[-1]
+
+
+# -------------------------------------------
+
+
+class ColumnTestCase(unittest.TestCase):
+    def testColumn(self):
+        Column(Direction.DESCENDING)
+
+
+if __name__ == "__main__":
+    unittest.main()
