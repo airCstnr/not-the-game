10fe5efc5517ec9edece60b50ea48b65c3c4808b
Raphaël Castanier <raphael.castanier@free.fr>
2023-08-11 16:39:12 +0200

    feat: fill hand

--- src/hand.py
+++ src/hand.py
@@ -1,16 +1,27 @@
 # Hand
 #
 # A hand can have maximum 6 Cards
+
 import unittest
 
 from card import Card
+from deck import Deck
+from state import State
 
 
 class Hand:
     cards = list()
 
     def draw(self, card: Card) -> None:
+        card.state = State.DRAWN
         self.cards.append(card)
+
+    def count(self) -> int:
+        return len(self.cards)
+
+    def fill(self, deck: Deck):
+        while self.count() < 6:
+            self.draw(deck.pick())
 
 
 # -------------------------------------------
