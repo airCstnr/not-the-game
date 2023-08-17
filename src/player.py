8208f3c0d834f0a348eb627a37bcf6ea5b3f4d81
Raphaël Castanier <raphael.castanier@free.fr>
2023-08-17 11:26:25 +0200

    feat: add player

---
+++ src/player.py
@@ -0,0 +1,10 @@
+# Player
+#
+# At its turn, a Player must play 2 Cards or 1 if the Deck is empty.
+# If the Player can't play a Card, the Game is over.
+# After playing, the Player fills its hand.
+# If all Cards are played, the Game is won.
+
+
+class Player:
+    pass
