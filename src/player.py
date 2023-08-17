9761f9e7afb5cc390a1e34342f31ada33c88e3f8
Raphaël Castanier <raphael.castanier@free.fr>
2023-08-17 11:38:21 +0200

    feat: implement player turn

--- src/player.py
+++ src/player.py
@@ -5,6 +5,18 @@
 # After playing, the Player fills its hand.
 # If all Cards are played, the Game is won.
 
+from renderer import Renderer
+from hand import Hand
+
 
 class Player:
-    pass
+    deck = None
+    
+    def __init__(self, index: int, hand: Hand) -> None:
+        self.index = index
+        self.hand = hand
+
+    def playTurn(self) -> None:
+        Renderer.renderPlayerHand(self.index, self.hand.count())
+        self.hand.fill(self.deck)
+        Renderer.renderPlayerHand(self.index, self.hand.count())
