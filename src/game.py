26378f32b3e431564ba87017a80a6a54e8d7b437
Raphaël Castanier <raphael.castanier@free.fr>
2023-08-17 11:38:59 +0200

    feat: use player turn in game

--- src/game.py
+++ src/game.py
@@ -3,8 +3,7 @@
 # The Game is composed of 100 Cards shuffled in the Deck.
 # Each Player (3 to 5) has one Hand with up to 6 Cards.
 # There are 4 Columns: 2 Ascending, 2 Descending.
-#
-#
+
 
 import unittest
 
@@ -14,6 +13,7 @@ from deck import Deck
 from direction import Direction
 from hand import Hand
 from renderer import Renderer
+from player import Player
 
 PLAYERS_COUNT = 5
 CARDS_COUNT = 10
@@ -24,11 +24,12 @@ class Game:
     def __init__(self) -> None:
         self.cards = self.generateCards()
         self.deck = Deck(self.cards)
+        Player.deck = self.deck
         self.players = list()
-        for _ in range(PLAYERS_COUNT):
+        for index in range(PLAYERS_COUNT):
             hand = Hand(MAXIMUM_CARDS_IN_HAND)
             hand.fill(self.deck)
-            self.players.append(hand)
+            self.players.append(Player(index + 1, hand))
         self.columns = [
             Column(Direction.ASCENDING),
             Column(Direction.ASCENDING),
@@ -39,31 +40,15 @@ class Game:
     def generateCards(self) -> list:
         return list(Card(x) for x in range(1, CARDS_COUNT + 1))
 
-    def playTurn(self, current_player_index: int) -> None:
-        current_player = self.players[current_player_index]
-
-        print(
-            f"Player {current_player_index + 1 } has "
-            f"{current_player.count()} cards."
-        )
-
-        current_player.fill(self.deck)
-
-        print(
-            f"Player {current_player_index + 1 } has "
-            f"{current_player.count()} cards."
-        )
-
-        print()
-
     def start(self):
         Renderer.renderBanner()
 
         print(f"Players : {PLAYERS_COUNT}")
         print()
 
-        for current_player_index in range(PLAYERS_COUNT):
-            self.playTurn(current_player_index)
+        for current_player in self.players:
+            current_player.playTurn()
+            print()
 
         print("------------------------")
 
